f64a240584e42e6be8325f9346989cbb1f8df965
poonwu <poon.wuthi@gmail.com>
2016-06-04 20:05:52 +0700

    Changed minor gulpfile.js description

--- gulpfile.js
+++ gulpfile.js
@@ -4,21 +4,22 @@
  * @author     Poon Wu <poon.wuthi@gmail.com>
  * @since      0.0.1
  */
-
+'use strict';
 var _ = require('lodash'),
 	gulp = require('gulp-help')(require('gulp')),
 	gutil = require('gulp-util'),
 	path = require('path'),
-	exec = require('child_process').exec;
+	exec = require('child_process').exec,
+    plugins = require('gulp-load-plugins')({
+        pattern: ['gulp-*', 'gulp.*'],
+        replaceString: /\bgulp[\-.]/
+    });
 
 // lint
 gulp.task('lint', 'Lint all server side js', function() {
-	var jshint = require('gulp-jshint');
-	var stylish = require('jshint-stylish');
-	
-	gulp.src('./api/**/*.js')
-		.pipe(jshint())
-		.pipe(jshint.reporter(stylish));
+	return gulp.src('./**/*.js')
+		.pipe(plugins.jshint())
+		.pipe(plugins.jshint.reporter(require('jshint-stylish')));
 })
 
 // run express-swagger app
@@ -31,7 +32,7 @@ gulp.task('start', 'Start express app', function(cb) {
 });
 
 // run swagger edit mode
-gulp.task('edit', 'Start swagger yaml edit mode', function(cb) {
+gulp.task('edit', 'Start swagger edit mode', function(cb) {
 	var task = exec('swagger project edit', {}, function(err) {
 		cb(err);
 	});
