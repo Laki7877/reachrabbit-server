915bbd8a50fcc83a1bdf1d0703a6310a1492f5f8
poonwu <poon.wuthi@gmail.com>
2016-06-06 23:20:44 +0700

    Added JWT swagger security handler

--- api/auth/passport.js
+++ api/auth/passport.js
@@ -9,15 +9,22 @@
 var passport = require('passport'),
     JwtStrategy = require('passport-jwt').Strategy,
     JwtExtractor = require('passport-jwt').ExtractJwt,
-	User = require('./model').User;
+	User = require('../models').User;
 
 module.exports = function(app, config) {
-  // handle swagger config
+  // handle swagger security
   config.swaggerSecurityHandler = {
     jwt: function(req, def, scopes, done) {
-      passport.authenticate('jwt', { session: false } , function(req, res) {
-
-      });
+      passport.authenticate('jwt', { session: false } , function(err, user, info) {
+        if(err) {
+          return done(new errors.httpStatusError(httpStatus.UNAUTHORIZED, 'Token is invalid'));
+        } else if(!user) {
+          return done(new errors.httpStatusError(httpStatus.UNAUTHORIZED, 'Token is invalid'));
+        } else {
+          req.user = user;
+          return done();
+        }
+      })(req, null, done);
     }
   };
 
@@ -25,18 +32,18 @@ module.exports = function(app, config) {
 	app.use(passport.initialize());
 	app.use(passport.session());
 
-    /**
-     * Json web token strategy
-     *
-     * @description Handle authentication with json web token strategy. User is required to attach "Bearer <token>" to Authorization Header in each authorized endpoint request.
-     * @see        https://github.com/themikenicholson/passport-jwt
-     */
-    passport.use(new JwtStrategy({
-        secretOrKey: process.env.JWT_SECRET || 'mySecretKey',
-        jwtFromRequest: JwtExtractor.fromAuthHeaderWithScheme('Bearer'),
-    }, function(payload, done) {
-        User.findOne({id: payload.sub}, function(user) {
-            done(null, user);
-        }, done);
-    }));
+  /**
+   * Json web token strategy
+   *
+   * @description Handle authentication with json web token strategy. User is required to attach "Bearer <token>" to Authorization Header in each authorized endpoint request.
+   * @see        https://github.com/themikenicholson/passport-jwt
+   */
+  passport.use(new JwtStrategy({
+      secretOrKey: process.env.JWT_SECRET || 'mySecretKey',
+      jwtFromRequest: JwtExtractor.fromAuthHeaderWithScheme('Bearer'),
+  }, function(payload, done) {
+      User.findOne({id: payload.id}, function(user) {
+          done(null, user);
+      }, done);
+  }));
 }
