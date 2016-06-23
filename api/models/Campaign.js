bdb4445a979b34792d0823369cf03f85d2d7c6ba
poonwu <poon.wuthi@gmail.com>
2016-06-23 21:51:48 +0700

    Added schema to database

---
+++ api/models/Campaign.js
@@ -0,0 +1,59 @@
+/**
+ * Campaign model
+ *
+ * @author     Poon Wu <poon.wuthi@gmail.com>
+ * @since      0.0.2
+ */
+'use strict';
+
+module.exports = function(sequelize, DataTypes) {
+  var Campaign = sequelize.define('Campaign', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true
+    },
+    title: DataTypes.STRING,
+    description: {
+      type: DataTypes.TEXT,
+      length: 'long'
+    },
+    proposalDeadline: {
+      type: DataTypes.DATE
+    },
+    submissionDeadline: {
+      type: DataTypes.DATE
+    },
+    status: DataTypes.ENUM('draft', 'open', 'production', 'complete')
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // brand owner
+        Campaign.belongsTo(models.User, {
+          foreignKey: 'ownerId'
+        });
+
+        // application
+        Campaign.belongsToMany(models.User, {
+          foreignKey: 'campaignId',
+          otherKey: 'userId',
+          scope: {
+            role: 'inf'
+          },
+          through: models.CampaignApplication
+        });
+
+        // submission
+        Campaign.belongsToMany(models.User, {
+          foreignKey: 'campaignId',
+          otherKey: 'userId',
+          scope: {
+            role: 'inf'
+          },
+          through: models.CampaignSubmission
+        });
+      }
+    }
+  });
+  return Campaign;
+};
