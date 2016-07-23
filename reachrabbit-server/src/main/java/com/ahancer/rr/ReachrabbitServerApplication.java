83f3471fdc7ad5775b523e572f2cc95a081ca4a0
Laki Sik <laki7877@outlook.com>
2016-07-23 14:32:14 +0700

    initiate new spring boot

---
+++ reachrabbit-server/src/main/java/com/ahancer/rr/ReachrabbitServerApplication.java
@@ -0,0 +1,12 @@
+package com.ahancer.rr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReachrabbitServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReachrabbitServerApplication.class, args);
+	}
+}
