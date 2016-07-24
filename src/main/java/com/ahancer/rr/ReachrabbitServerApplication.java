04e513aef098a31ffeaee0f83d2a4787b2d12cae
Laki Sik <laki7877@outlook.com>
2016-07-24 15:47:21 +0700

    enable cors

--- src/main/java/com/ahancer/rr/ReachrabbitServerApplication.java
+++ src/main/java/com/ahancer/rr/ReachrabbitServerApplication.java
@@ -2,9 +2,25 @@ package com.ahancer.rr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class ReachrabbitServerApplication {
+	
+	
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("*").allowedOrigins("*").allowedMethods("*");
+            }
+        };
+    }
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReachrabbitServerApplication.class, args);
