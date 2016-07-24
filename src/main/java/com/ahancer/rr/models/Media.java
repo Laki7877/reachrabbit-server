5d7929c64af208e3936e5ae2c415d00ad6288cc8
Laki Sik <laki7877@outlook.com>
2016-07-24 11:17:56 +0700

    add common lang library

--- src/main/java/com/ahancer/rr/models/Media.java
+++ src/main/java/com/ahancer/rr/models/Media.java
@@ -1,13 +1,10 @@
 package com.ahancer.rr.models;
 
-import java.util.List;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
 
 @Entity(name="media")
 public class Media {
