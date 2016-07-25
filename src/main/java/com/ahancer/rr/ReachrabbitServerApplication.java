9bf9cc96d29486149a22dc53f47b4e4ca30aaede
Poon <poon.wuthi@gmail.com>
2016-07-25 16:46:48 +0700

    Added swagger ui

--- src/main/java/com/ahancer/rr/ReachrabbitServerApplication.java
+++ src/main/java/com/ahancer/rr/ReachrabbitServerApplication.java
@@ -4,24 +4,52 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class ReachrabbitServerApplication {
-	
-	
 	@Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurerAdapter() {
+        	
+        	@Override
+        	public void addViewControllers(ViewControllerRegistry registry) {
+        	    registry.addRedirectViewController("/doc", "/doc/index.html");
+        	    registry.addRedirectViewController("/doc/", "/doc/index.html");
+        	}
+        	
+        	@Override
+            public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+                registry.addResourceHandler("/doc/**").addResourceLocations("classpath:/swaggerui/");
+                registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+            }
+        	
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("*").allowedOrigins("*").allowedMethods("*");
+                
             }
         };
     }
-	
-
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build()
+				.pathMapping("/");
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ReachrabbitServerApplication.class, args);
 	}
