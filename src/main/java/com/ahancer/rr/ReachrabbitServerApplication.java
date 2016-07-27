459b792e770741fa8552a0f4ce97b76c395d2eaa
Poon <poon.wuthi@gmail.com>
2016-07-27 18:17:12 +0700

    Revert to swagger-ui package

--- src/main/java/com/ahancer/rr/ReachrabbitServerApplication.java
+++ src/main/java/com/ahancer/rr/ReachrabbitServerApplication.java
@@ -3,15 +3,35 @@ package com.ahancer.rr;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-@EnableWebMvc
+@EnableSwagger2
 @ComponentScan
 public class ReachrabbitServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ReachrabbitServerApplication.class, args);
+	}
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build()
+				.pathMapping("/");
 	}
 }
--- src/main/java/com/ahancer/rr/SwaggerConfig.java
+++ src/main/java/com/ahancer/rr/SwaggerConfig.java
@@ -15,32 +15,5 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurerAdapter() {
-
-			@Override
-			public void addViewControllers(ViewControllerRegistry registry) {
-				registry.addRedirectViewController("/doc", "/doc/index.html");
-				registry.addRedirectViewController("/doc/", "/doc/index.html");
-			}
-
-			@Override
-			public void addResourceHandlers(final ResourceHandlerRegistry registry) {
-				registry.addResourceHandler("/doc/**").addResourceLocations("classpath:/swaggerui/");
-				registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
-			}
-		};
-	}
-	@Bean
-	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2)
-				.select()
-				.apis(RequestHandlerSelectors.any())
-				.paths(PathSelectors.any())
-				.build()
-				.pathMapping("/");
-	}
 }
