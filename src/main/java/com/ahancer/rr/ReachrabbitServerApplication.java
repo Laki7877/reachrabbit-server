6585f2a541f45d701ca848c8f9c7f992d18abaad
Laki Sik <laki7877@outlook.com>
2016-07-27 14:27:33 +0700

    add messages.properties and error handler

--- src/main/java/com/ahancer/rr/ReachrabbitServerApplication.java
+++ src/main/java/com/ahancer/rr/ReachrabbitServerApplication.java
@@ -3,14 +3,17 @@ package com.ahancer.rr;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @ComponentScan
 public class ReachrabbitServerApplication {
 	
-
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ReachrabbitServerApplication.class, args);
 	}
--- src/main/java/com/ahancer/rr/exception/ResponseExceptionHandler.java
+++ src/main/java/com/ahancer/rr/exception/ResponseExceptionHandler.java
@@ -1,20 +1,39 @@
 package com.ahancer.rr.exception;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class ResponseExceptionHandler extends ResponseEntityExceptionHandler {
+@PropertySource("messages.properties")
+public class ResponseExceptionHandler  {
 
+	@Autowired
+	private Environment env;
+	
 	@ExceptionHandler(Exception.class)
-    public ResponseEntity<?> exceptionHandler(HttpServletRequest request, HttpServletResponse response, Exception ex) {
-        ex.printStackTrace();
-        ResponseException responseEx = (ex instanceof ResponseException) ? (ResponseException)ex : new ResponseException(ex.getMessage());
-        return new ResponseEntity<>(responseEx.getResponseMessage(), responseEx.getStatusCode());
-    }
+	public ResponseEntity<?> processValidationError(Exception ex) {
+		ResponseException responseEx = null;
+		if(ex instanceof MethodArgumentNotValidException) {
+			MethodArgumentNotValidException exception = (MethodArgumentNotValidException)ex;
+			BindingResult result = exception.getBindingResult();
+			FieldError error = result.getFieldError();
+			String msg = env.getProperty(error.getDefaultMessage());
+			responseEx = new ResponseException(msg, HttpStatus.BAD_REQUEST);
+		}else if(ex instanceof ResponseException) {
+			responseEx = (ResponseException)ex;
+			responseEx.setMessage(env.getProperty(responseEx.getMessage()));
+		}else {
+			responseEx = new ResponseException(ex.getMessage()); 
+		}
+		return new ResponseEntity<>(responseEx.getResponseMessage(), responseEx.getStatusCode());
+	}
+	
 }
