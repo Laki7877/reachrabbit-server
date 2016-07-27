5e2e6d33470cf15d7d5d8f642c7e84b61b1170dd
Poon <poon.wuthi@gmail.com>
2016-07-28 00:39:16 +0700

    Readjust swagger

--- src/main/java/com/ahancer/rr/ReachrabbitServerApplication.java
+++ src/main/java/com/ahancer/rr/ReachrabbitServerApplication.java
@@ -8,8 +8,13 @@ import org.springframework.context.annotation.ComponentScan;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.ApiKeyVehicle;
+import springfox.documentation.swagger.web.SecurityConfiguration;
+import springfox.documentation.swagger.web.UiConfiguration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -28,5 +33,24 @@ public class ReachrabbitServerApplication {
 				.paths(PathSelectors.any())
 				.build()
 				.pathMapping("/");
+				//.securitySchemes(Arrays.asList(apiKey()));
 	}
+	
+	public SecurityScheme apiKey() {
+		return new ApiKey("X-Auth-Token", "X-Auth-Token", "header");
+	}
+	@Bean
+	public SecurityConfiguration securityInfo() {
+		return new SecurityConfiguration("","","","reachrabbit","",ApiKeyVehicle.HEADER, "X-Auth-Token", ",");
+	}
+   @Bean
+   public UiConfiguration uiConfig() {
+	    return new UiConfiguration(
+	        null,// url
+	        "list",       // docExpansion          => none | list
+	        "alpha",      // apiSorter             => alpha
+	        "schema",     // defaultModelRendering => schema
+	        false,        // enableJsonEditor      => true | false
+	        true);        // showRequestHeaders    => true | false
+  	}
 }
