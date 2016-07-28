5e50ceacea1200d90345d00f3e88ce41faf0219e
Poon Wu <poon.wuthi@gmail.com>
2016-07-28 18:07:35 +0700

    Completed test flow

--- src/test/java/com/ahancer/rr/controllers/AbstractIT.java
+++ src/test/java/com/ahancer/rr/controllers/AbstractIT.java
@@ -5,13 +5,23 @@ import java.util.Arrays;
 import org.junit.After;
 import org.junit.Before;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 
 import com.ahancer.rr.custom.type.Role;
 import com.ahancer.rr.daos.UserDao;
 import com.ahancer.rr.models.User;
 import com.ahancer.rr.utils.EncryptionUtil;
 
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.config.LogConfig;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+
 public abstract class AbstractIT {
+
+    @Value("${server.port}") 
+    private int port;
 	
 	@Autowired
 	private UserDao userDao;
@@ -19,8 +29,23 @@ public abstract class AbstractIT {
 	@Autowired
 	private EncryptionUtil encryptionUtil;
 	
+	protected String adminToken;
+	protected String brandToken;
+	protected String influencerToken;
+	
+	public void setupRestAssured() {
+		LogConfig logConfig = LogConfig.logConfig().enableLoggingOfRequestAndResponseIfValidationFails(LogDetail.ALL).enablePrettyPrinting(true);
+		
+		RestAssured.port = port;
+		RestAssured.config = RestAssured.config()
+			.logConfig(logConfig);
+		RestAssured.requestSpecification = new RequestSpecBuilder().setContentType(ContentType.JSON).build();
+	}
+	
 	@Before
 	public void before() {
+		//Setup test framework
+		setupRestAssured();
 		
 		//Create admin
 		User admin = new User();
--- src/test/java/com/ahancer/rr/controllers/AuthenticationControllerIT.java
+++ src/test/java/com/ahancer/rr/controllers/AuthenticationControllerIT.java
@@ -1,4 +1,7 @@
 package com.ahancer.rr.controllers;
+
+import static io.restassured.RestAssured.given;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
@@ -6,13 +9,24 @@ import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.ahancer.rr.ReachrabbitServerApplication;
+import com.ahancer.rr.models.User;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = ReachrabbitServerApplication.class)
 @WebIntegrationTest
-public class UserControllerIT extends AbstractIT {
+public class AuthenticationControllerIT extends AbstractIT{
 	@Test
-	public void findUser() {
-		System.out.println("test");
+	public void loginWithWrongUser() {
+		User user = new User();
+		user.setEmail("wrong@email.com");
+		user.setPassword("wrong");
+		
+		//Call endpoint
+		given()
+			.body(user)
+		.expect()
+			.statusCode(400)
+		.when()
+			.post("/auth/login");
 	}
 }
