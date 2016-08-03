5eec4b3478bb57f04b2c6757b104b6764e72b7ef
Laki Sik <laki7877@outlook.com>
2016-08-03 22:05:27 +0700

    add mapid field

--- src/main/java/com/ahancer/rr/models/Brand.java
+++ src/main/java/com/ahancer/rr/models/Brand.java
@@ -26,7 +26,7 @@ public class Brand implements Serializable{
 	@Column(name="brandId",unique = true, nullable = false)
 	private Long brandId;
 
-	@MapsId
+	@MapsId("brandId")
 	@OneToOne
     @JoinColumn(name = "brandId")
 	private User user;
