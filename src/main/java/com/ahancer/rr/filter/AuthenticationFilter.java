0a71fccdc3d9d58cda9b7c7b99daf3aa7d55ecb4
Laki Sik <laki7877@outlook.com>
2016-08-04 17:30:17 +0700

    fix option method authen

--- src/main/java/com/ahancer/rr/filter/AuthenticationFilter.java
+++ src/main/java/com/ahancer/rr/filter/AuthenticationFilter.java
@@ -49,6 +49,7 @@ public class AuthenticationFilter implements Filter {
 		HttpServletResponse response = (HttpServletResponse) res;
 		HttpServletRequest request = (HttpServletRequest)req;
 		String path = request.getRequestURI();
+		String method = request.getMethod().toLowerCase();
 		if (path.startsWith("/auth/") 
 				|| path.startsWith("/signup")
 				|| path.startsWith("/webjar") 
@@ -56,7 +57,8 @@ public class AuthenticationFilter implements Filter {
 				|| path.startsWith("/swagger-ui") 
 				|| path.startsWith("/swagger-resources") 
 				|| path.startsWith("/configuration") 
-				|| path.startsWith("/v2")) {
+				|| path.startsWith("/v2")
+				|| "option".equals(method)) {
 			chain.doFilter(req, res);
 		} else {
 			try {
