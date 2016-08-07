83ab1d458c4c92cda262514f8985db217a20d59e
Laki Sik <laki7877@outlook.com>
2016-08-07 10:41:49 +0700

    Fix option method and /data/budge and seed google primary key to youtube

--- src/main/java/com/ahancer/rr/controllers/DataController.java
+++ src/main/java/com/ahancer/rr/controllers/DataController.java
@@ -6,9 +6,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ahancer.rr.daos.BankDao;
+import com.ahancer.rr.daos.BudgetDao;
 import com.ahancer.rr.daos.CategoryDao;
 import com.ahancer.rr.daos.MediaDao;
 import com.ahancer.rr.models.Bank;
+import com.ahancer.rr.models.Budget;
 import com.ahancer.rr.models.Category;
 import com.ahancer.rr.models.Media;
 
@@ -24,6 +26,9 @@ public class DataController {
 	
 	@Autowired
 	private BankDao bankDao;
+	
+	@Autowired
+	private BudgetDao budgetDao;
 
 	@RequestMapping(value="/media",method=RequestMethod.GET)
 	public Iterable<Media> getAllMedia() throws Exception{
@@ -38,6 +43,11 @@ public class DataController {
 	@RequestMapping(value="/banks",method=RequestMethod.GET)
 	public Iterable<Bank> getAllBanks() throws Exception{
 		return bankDao.findAll();
+	}
+	
+	@RequestMapping(value="/budget",method=RequestMethod.GET)
+	public Iterable<Budget> getBudget() throws Exception{
+		return budgetDao.findAll();
 	}
 	
 }
