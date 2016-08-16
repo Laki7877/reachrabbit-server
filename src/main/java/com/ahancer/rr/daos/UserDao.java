b8bcbc1b753b04bbe5a9eb418aa6919d06b85705
Laki Sik <laki7877@outlook.com>
2016-08-16 16:47:42 +0700

    Revert "fix user query"
    This reverts commit 4173276c102b6f9c11d9842f16524f76a37704fb.

--- src/main/java/com/ahancer/rr/daos/UserDao.java
+++ src/main/java/com/ahancer/rr/daos/UserDao.java
@@ -12,10 +12,10 @@ public interface UserDao extends CrudRepository<User, Long> {
 	public User findByEmail(String email);
 	
 	@Query("SELECT u FROM user u"
-			+ " LEFT JOIN u.profilePicture r"
+			+ " JOIN u.profilePicture r"
 			+ " JOIN u.influencer i"
 			+ " JOIN i.influencerMedias im"
-			+ " LEFT JOIN i.categories ic"
+			+ " JOIN i.categories ic"
 			+ " WHERE im.influencerMediaId.mediaId=:mediaId AND im.socialId=:socialId")
 	public User findBySocialIdAndMediaId(@Param("mediaId") String mediaId,@Param("socialId") String socialId);
 
