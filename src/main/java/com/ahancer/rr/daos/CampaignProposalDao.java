4c6e179aa0f58a8676d075a85c5a83e4b25cee1b
Laki Sik <laki7877@outlook.com>
2016-08-17 13:05:37 +0700

    post campaign proposal

--- src/main/java/com/ahancer/rr/daos/CampaignProposalDao.java
+++ src/main/java/com/ahancer/rr/daos/CampaignProposalDao.java
@@ -2,11 +2,16 @@ package com.ahancer.rr.daos;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.ahancer.rr.models.CampaignProposal;
 
 public interface CampaignProposalDao extends CrudRepository<CampaignProposal, Long> {
-	
-	Page<CampaignProposal> findAll(Pageable pageable);
+
+	public Page<CampaignProposal> findAll(Pageable pageable);
+
+	@Query("SELECT COUNT(cp) FROM campaignProposal cp WHERE cp.influencer.influencerId=:influencerId AND cp.campaign.campaignId=:campaignId")
+	public int countByInfluencerAndCampaign(@Param("influencerId") Long influencerId, @Param("campaignId") Long campaignId);
 }
--- src/main/java/com/ahancer/rr/services/CampaignProposalService.java
+++ src/main/java/com/ahancer/rr/services/CampaignProposalService.java
@@ -3,10 +3,13 @@ package com.ahancer.rr.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.ahancer.rr.daos.CampaignProposalDao;
+import com.ahancer.rr.exception.ResponseException;
+import com.ahancer.rr.models.Campaign;
 import com.ahancer.rr.models.CampaignProposal;
 import com.ahancer.rr.models.Influencer;
 
@@ -21,7 +24,12 @@ public class CampaignProposalService {
 		return campaignProposalDao.findAll(pageable);
 	}
 	
-	public CampaignProposal createCampaignProposalByInfluencer(CampaignProposal proposal,Influencer influencer){
+	public CampaignProposal createCampaignProposalByInfluencer(CampaignProposal proposal,Influencer influencer) throws Exception {
+		Campaign campaign = proposal.getCampaign();
+		int count = campaignProposalDao.countByInfluencerAndCampaign(influencer.getInfluencerId(), campaign.getCampaignId());
+		if(0 < count){
+			throw new ResponseException(HttpStatus.BAD_REQUEST,"error.campaign.already.proposal");
+		}
 		proposal.setInfluencer(influencer);
 		proposal = campaignProposalDao.save(proposal);
 		return proposal;
