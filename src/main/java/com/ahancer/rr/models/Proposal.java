fb80cea072d5778bbff2f0d0f6fe73e76b0e4572
Laki Sik <laki7877@outlook.com>
2016-08-18 15:54:13 +0700

    fix @Transient @JsonSerialize @JsonDeserialize

--- src/main/java/com/ahancer/rr/models/Proposal.java
+++ src/main/java/com/ahancer/rr/models/Proposal.java
@@ -16,8 +16,11 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.Transient;
 
 import com.ahancer.rr.custom.type.ProposalStatus;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 @Entity(name="proposal")
 public class Proposal extends AbstractModel implements Serializable{
@@ -47,6 +50,10 @@ public class Proposal extends AbstractModel implements Serializable{
 			inverseJoinColumns=@JoinColumn(name="mediaId", referencedColumnName="mediaId"))
 	private Set<Media> media = new HashSet<Media>(0);
 	
+	
+	@Transient
+	@JsonSerialize
+	@JsonDeserialize
 	private String description;
 	
 	@Column(name="proposePrice",scale=10,precision=3)
