df3907ff7e912ed0a9cdd16f904a9d8df676706c
Laki Sik <laki7877@outlook.com>
2016-08-19 22:10:14 +0700

    fix post message by brand

--- src/main/java/com/ahancer/rr/daos/ProposalDao.java
+++ src/main/java/com/ahancer/rr/daos/ProposalDao.java
@@ -38,12 +38,15 @@ public interface ProposalDao extends CrudRepository<Proposal, Long> {
 	@Query("SELECT COUNT(cp) FROM proposal cp WHERE cp.influencer.influencerId=:influencerId AND cp.campaign.campaignId=:campaignId")
 	public int countByInfluencerAndCampaign(@Param("influencerId") Long influencerId, @Param("campaignId") Long campaignId);
 	
+	@Query("SELECT COUNT(cp) FROM proposal cp WHERE proposalId=:proposalId AND cp.campaign.brandId=:brandId")
+	public int countByBrand(@Param("proposalId") Long proposalId,@Param("brandId") Long brandId);
+	
 	@Modifying
 	@Query("UPDATE proposal cp SET messageUpdatedAt=:messageUpdatedAt WHERE cp.proposalId=:proposalId AND cp.influencerId=:influencerId")
 	public int updateMessageUpdatedAtByInfluencer(@Param("proposalId") Long proposalId,  @Param("influencerId") Long influencerId, @Param("messageUpdatedAt") Date messageUpdatedAt);
 	
 	@Modifying
-	@Query("UPDATE proposal cp SET messageUpdatedAt=:messageUpdatedAt WHERE cp.proposalId=:proposalId AND cp.campaign.brandId=:brandId")
-	public int updateMessageUpdatedAtByBrand(@Param("proposalId") Long proposalId,  @Param("brandId") Long brandId, @Param("messageUpdatedAt") Date messageUpdatedAt);
+	@Query("UPDATE proposal cp SET messageUpdatedAt=:messageUpdatedAt WHERE cp.proposalId=:proposalId")
+	public int updateMessageUpdatedAtByBrand(@Param("proposalId") Long proposalId, @Param("messageUpdatedAt") Date messageUpdatedAt);
 	
 }
