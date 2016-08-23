f89dda447cf138a52c6262df98c8980b66db6c6b
Laki Sik <laki7877@outlook.com>
2016-08-23 16:35:37 +0700

    fix campaign resource

--- src/main/java/com/ahancer/rr/models/Campaign.java
+++ src/main/java/com/ahancer/rr/models/Campaign.java
@@ -57,7 +57,7 @@ public class Campaign extends AbstractModel implements Serializable {
 	@OrderBy("mediaId")
 	private Set<Media> media = new HashSet<Media>(0);
 
-	@OneToMany(fetch = FetchType.EAGER, mappedBy = "campaign",cascade=CascadeType.ALL)
+	@OneToMany(fetch = FetchType.EAGER, mappedBy = "campaign")
 	@OrderBy("position")
 	private Set<CampaignResource> campaignResources = new HashSet<CampaignResource>(0);
 	
