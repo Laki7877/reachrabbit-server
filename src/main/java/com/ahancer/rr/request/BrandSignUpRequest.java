3e38b98436d2fbf37937f9781b1076909e784c5a
Laki Sik <laki7877@outlook.com>
2016-09-05 10:46:59 +0700

    fix validation

--- src/main/java/com/ahancer/rr/request/BrandSignUpRequest.java
+++ src/main/java/com/ahancer/rr/request/BrandSignUpRequest.java
@@ -22,7 +22,7 @@ public class BrandSignUpRequest implements Serializable {
 	
 	@NotNull(message="error.phonenumber.require")
 	@Size(min=1,max=20,message="error.phonenumber.length")
-	@Pattern(regexp="[\\d]{20}",message="error.phonenumber.number.only")
+	@Pattern(regexp="(^$|[0-9]{10})",message="error.phonenumber.number.only")
 	private String phoneNumber;
 	
 	@NotNull(message="error.password.require")
