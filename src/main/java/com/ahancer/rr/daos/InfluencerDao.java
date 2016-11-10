fa28c57959ebba902da2080e20ffd95c501f9c4d
Laki Sik <laki7877@gmail.com>
2016-11-10 11:38:05 +0700

    fix infleucner proposal commission

--- src/main/java/com/ahancer/rr/daos/InfluencerDao.java
+++ src/main/java/com/ahancer/rr/daos/InfluencerDao.java
@@ -1,6 +1,8 @@
 package com.ahancer.rr.daos;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.ahancer.rr.models.Influencer;
@@ -8,6 +10,9 @@ import com.ahancer.rr.models.Influencer;
 @Repository
 public interface InfluencerDao extends CrudRepository<Influencer, Long> {
 	
-	public Double findCommissionByInfluencerId(Long influencerId);
+	@Query("SELECT i.commission "
+			+ "FROM influencer i "
+			+ "WHERE i.influencerId = :influencerId")
+	public Double findCommissionByInfluencerId(@Param("influencerId") Long influencerId);
 	
 }
